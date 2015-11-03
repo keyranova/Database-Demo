610e1f6cde793559031dd556aea96dae687e75a8
kurtnovack <kurt@walltowall.com>
2015-11-03 17:28:03 -0500

    adds project files

---
+++ app/src/main/java/com/kurtnovack/databasedemo/MainActivity.java
@@ -0,0 +1,65 @@
+package com.kurtnovack.databasedemo;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        try {
+            SQLiteDatabase newDatabase = this.openOrCreateDatabase("Events", MODE_PRIVATE, null);
+
+            newDatabase.execSQL("CREATE TABLE IF NOT EXISTS events (title VARCHAR, date VARCHAR)");
+
+            newDatabase.execSQL("INSERT INTO events (title, date) VALUES ('birth', 'December 14, 1988')");
+            newDatabase.execSQL("INSERT INTO events (title, date) VALUES ('today', 'October 30, 2015')");
+
+            Cursor c = newDatabase.rawQuery("SELECT * FROM events", null);
+
+            int titleIndex = c.getColumnIndex("title");
+            int dateIndex = c.getColumnIndex("date");
+
+            c.moveToFirst();
+            while (c != null) {
+                Log.i("title", c.getString(titleIndex));
+                Log.i("date", c.getString(dateIndex));
+
+                c.moveToNext();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        try {
+//            SQLiteDatabase myDatabase = this.openOrCreateDatabase("Users", MODE_PRIVATE, null);
+//
+//            myDatabase.execSQL("CREATE TABLE IF NOT EXISTS users (name VARCHAR, age INT(3))");
+//
+//            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Rob', 34)");
+//            myDatabase.execSQL("INSERT INTO users (name, age) VALUES ('Kurt', 26)");
+//
+//            Cursor c = myDatabase.rawQuery("SELECT * FROM users", null);
+//
+//            int nameIndex = c.getColumnIndex("name");
+//            int ageIndex = c.getColumnIndex("age");
+//
+//            c.moveToFirst();
+//            while (c != null) {
+//                Log.i("name", c.getString(nameIndex));
+//                Log.i("age", Integer.toString(c.getInt(ageIndex)));
+//
+//                c.moveToNext();
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
